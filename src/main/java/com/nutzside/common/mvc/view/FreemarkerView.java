cf5ae2e004510288e677651f5d3e2a2d49b9d657
xwinie <xwinie@gmail.com>
2013-02-20 16:30:02 +0800

    去掉多余的freemarker类简化插件  登陆界面OVER

--- src/main/java/com/nutzside/common/mvc/view/FreemarkerView.java
+++ src/main/java/com/nutzside/common/mvc/view/FreemarkerView.java
@@ -1,13 +1,16 @@
 package com.nutzside.common.mvc.view;
 
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Properties;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletContext;
@@ -15,14 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.nutz.ioc.Ioc;
 import org.nutz.lang.Files;
 import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
 import org.nutz.mvc.Mvcs;
 import org.nutz.mvc.view.AbstractPathView;
-
-import com.nutzside.common.freemarker.FreeMarkerConfigurer;
 
 import freemarker.ext.jsp.TaglibFactory;
 import freemarker.ext.servlet.HttpRequestHashModel;
@@ -33,62 +33,64 @@ import freemarker.template.Configuration;
 import freemarker.template.ObjectWrapper;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 import freemarker.template.TemplateModel;
 
-
-public class FreemarkerView extends AbstractPathView{
+public class FreemarkerView extends AbstractPathView {
 	private static final String ATTR_APPLICATION_MODEL = ".freemarker.Application";
-    private static final String ATTR_JSP_TAGLIBS_MODEL = ".freemarker.JspTaglibs";
-    private static final String ATTR_REQUEST_MODEL = ".freemarker.Request";
-    private static final String ATTR_REQUEST_PARAMETERS_MODEL = ".freemarker.RequestParameters";    
-    private static final String KEY_APPLICATION = "Application";
-    private static final String KEY_REQUEST_MODEL = "Request";
-    private static final String KEY_SESSION_MODEL = "Session";    
-    private static final String KEY_REQUEST_PARAMETER_MODEL = "Parameters";
-    private static final String KEY_EXCEPTION = "exception";
-    private static final String OBJ = "obj";	
-    private static final String REQUEST = "request";
-    private static final String RESPONSE = "response";
-    private static final String SESSION = "session";
-    private static final String APPLICATION = "application";
-    private static final String KEY_JSP_TAGLIBS = "JspTaglibs";
-    public static final String PATH_BASE = "base";
+	private static final String ATTR_JSP_TAGLIBS_MODEL = ".freemarker.JspTaglibs";
+	private static final String ATTR_REQUEST_MODEL = ".freemarker.Request";
+	private static final String ATTR_REQUEST_PARAMETERS_MODEL = ".freemarker.RequestParameters";
+	private static final String KEY_APPLICATION = "Application";
+	private static final String KEY_REQUEST_MODEL = "Request";
+	private static final String KEY_SESSION_MODEL = "Session";
+	private static final String KEY_REQUEST_PARAMETER_MODEL = "Parameters";
+	private static final String KEY_EXCEPTION = "exception";
+	private static final String OBJ = "obj";
+	private static final String REQUEST = "request";
+	private static final String RESPONSE = "response";
+	private static final String SESSION = "session";
+	private static final String APPLICATION = "application";
+	private static final String KEY_JSP_TAGLIBS = "JspTaglibs";
+	public static final String PATH_BASE = "base";
 	private static final String CONFIG_SERVLET_CONTEXT_KEY = "freemarker.Configuration";
-	
-	public FreemarkerView(String path){
+	private Configuration cfg; 
+	public FreemarkerView(String path) {
 		super(path);
 	}
-	
-	public void render(HttpServletRequest request, HttpServletResponse response, Object value) throws Throwable {
+
+	@Override
+	public void render(HttpServletRequest request,
+			HttpServletResponse response, Object value) throws Throwable {
 		String $temp = evalPath(request, value);
-		String path = getPath($temp,request);
-		ServletContext sc = request.getSession().getServletContext();		
-		Ioc ioc = Mvcs.getIoc();
-		FreeMarkerConfigurer freeMarkerConfigurer = ioc.get(FreeMarkerConfigurer.class);
-		Configuration cfg = freeMarkerConfigurer.getConfiguration();
-		Map<String,Object> root = new HashMap<String,Object>();		
+		String path = getPath($temp, request);
+		ServletContext sc = request.getSession().getServletContext();
+		cfg = getConfiguration(sc);		
+		Map<String, Object> root = new HashMap<String, Object>();
 		root.put(OBJ, value);
 		root.put(REQUEST, request);
 		root.put(RESPONSE, response);
 		HttpSession session = request.getSession();
 		root.put(SESSION, session);
 		root.put(APPLICATION, sc);
-		root.put("system", System.getProperties());//.get("java.version")
-		Enumeration<?> reqs=request.getAttributeNames();
-		while(reqs.hasMoreElements()){
-			String strKey=(String)reqs.nextElement();
+		root.put("system", System.getProperties());// .get("java.version")
+		Enumeration<?> reqs = request.getAttributeNames();
+		while (reqs.hasMoreElements()) {
+			String strKey = (String) reqs.nextElement();
 			root.put(strKey, request.getAttribute(strKey));
 		}
-		jspTaglibs(sc,request,response,root,cfg.getObjectWrapper());
-		//cfg.setServletContextForTemplateLoading(request.getSession().getServletContext(), "/"); 
-		//模版路径
+		jspTaglibs(sc, request, response, root, cfg.getObjectWrapper());
+		// cfg.setServletContextForTemplateLoading(request.getSession().getServletContext(),
+		// "/");
+		// 模版路径
 		try {
 			Template template = cfg.getTemplate(path);
-			response.setContentType("text/html; charset="+template.getEncoding());		
+			response.setContentType("text/html; charset="
+					+ template.getEncoding());
 			template.process(root, response.getWriter());
-		}catch (TemplateException e) {
+		} catch (TemplateException e) {
 			e.printStackTrace();
-		}catch (UnsupportedEncodingException e) {
+		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -96,7 +98,7 @@ public class FreemarkerView extends AbstractPathView{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getWebRealPath(HttpServletRequest request) {
 		StringBuffer sb = new StringBuffer();
 		sb.append("http://");
@@ -109,6 +111,7 @@ public class FreemarkerView extends AbstractPathView{
 		sb.append("/");
 		return sb.toString();
 	}
+
 	/**
 	 * 子类可以覆盖这个方法，给出自己特殊的后缀
 	 * 
@@ -117,7 +120,7 @@ public class FreemarkerView extends AbstractPathView{
 	protected static String getExt() {
 		return ".ftl";
 	}
-	
+
 	private String getPath(String path, HttpServletRequest request) {
 		StringBuffer sb = new StringBuffer();
 		// 空路径，采用默认规则
@@ -140,46 +143,104 @@ public class FreemarkerView extends AbstractPathView{
 		}
 		return sb.toString();
 	}
+
+	
+	public final synchronized Configuration getConfiguration(ServletContext servletContext) throws TemplateException, IOException {
+        Configuration config = (Configuration) servletContext.getAttribute(CONFIG_SERVLET_CONTEXT_KEY);
+       
+        if (config == null) {
+        	config = new Configuration();        	
+        	config.setServletContextForTemplateLoading(servletContext, "/");
+        	//config.setDefaultEncoding("UTF-8");
+            config.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+            //读取freemarker配置文件
+            loadSettings(servletContext,config);
+            String path = Mvcs.getServletContext().getRealPath("WEB-INF");
+            config.setDirectoryForTemplateLoading(Files.findFile(path));
+            servletContext.setAttribute(CONFIG_SERVLET_CONTEXT_KEY, config);
+        }        
+        config.setWhitespaceStripping(true);        
+        return config;
+	}
 	
 	
-	protected void jspTaglibs(ServletContext servletContext,HttpServletRequest request,HttpServletResponse response, Map<String, Object> model,ObjectWrapper wrapper){
-		synchronized (servletContext) {
-            ServletContextHashModel servletContextModel = (ServletContextHashModel) servletContext.getAttribute(ATTR_APPLICATION_MODEL);
-            if (Lang.isEmpty(servletContextModel)) {
-                GenericServlet servlet = JspSupportServlet.jspSupportServlet;
-                if (!Lang.isEmpty(servlet)) {
-                    servletContextModel = new ServletContextHashModel(servlet, wrapper);
-                    servletContext.setAttribute(ATTR_APPLICATION_MODEL, servletContextModel);
-                    TaglibFactory taglibs = new TaglibFactory(servletContext);
-                    servletContext.setAttribute(ATTR_JSP_TAGLIBS_MODEL, taglibs);
+	
+	protected void loadSettings(ServletContext servletContext,Configuration config){		
+		InputStream in = null;
+        try {        	
+        	in=new BufferedInputStream(new FileInputStream(Files.findFile("freemarker.properties")));         	
+            if (in != null) {
+                Properties p = new Properties();
+                p.load(in);
+                config.setSettings(p);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+        	 e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch(IOException io) {
+                	io.printStackTrace();
                 }
             }
-            model.put(KEY_APPLICATION, servletContextModel);
-            TemplateModel tempModel = (TemplateModel) servletContext.getAttribute(ATTR_JSP_TAGLIBS_MODEL);
-            model.put(KEY_JSP_TAGLIBS, tempModel);
         }
+	}
+	protected void jspTaglibs(ServletContext servletContext,
+			HttpServletRequest request, HttpServletResponse response,
+			Map<String, Object> model, ObjectWrapper wrapper) {
+		synchronized (servletContext) {
+			ServletContextHashModel servletContextModel = (ServletContextHashModel) servletContext
+					.getAttribute(ATTR_APPLICATION_MODEL);
+			if (Lang.isEmpty(servletContextModel)) {
+				GenericServlet servlet = JspSupportServlet.jspSupportServlet;
+				if (!Lang.isEmpty(servlet)) {
+					servletContextModel = new ServletContextHashModel(servlet,
+							wrapper);
+					servletContext.setAttribute(ATTR_APPLICATION_MODEL,
+							servletContextModel);
+					TaglibFactory taglibs = new TaglibFactory(servletContext);
+					servletContext
+							.setAttribute(ATTR_JSP_TAGLIBS_MODEL, taglibs);
+				}
+			}
+			model.put(KEY_APPLICATION, servletContextModel);
+			TemplateModel tempModel = (TemplateModel) servletContext
+					.getAttribute(ATTR_JSP_TAGLIBS_MODEL);
+			model.put(KEY_JSP_TAGLIBS, tempModel);
+		}
 		HttpSession session = request.getSession(false);
-        if (!Lang.isEmpty(session)) {
-            model.put(KEY_SESSION_MODEL, new HttpSessionHashModel(session, wrapper));
-        }
-		HttpRequestHashModel requestModel = (HttpRequestHashModel) request.getAttribute(ATTR_REQUEST_MODEL);
-        if (Lang.isEmpty(requestModel) || !Lang.equals(requestModel.getRequest(), request)) {
-            requestModel = new HttpRequestHashModel(request, response, wrapper);
-            request.setAttribute(ATTR_REQUEST_MODEL, requestModel);
-        }
-        model.put(KEY_REQUEST_MODEL, requestModel);
-        HttpRequestParametersHashModel reqParametersModel = (HttpRequestParametersHashModel) request.getAttribute(ATTR_REQUEST_PARAMETERS_MODEL);
-        if (Lang.isEmpty(reqParametersModel) || !Lang.equals(requestModel.getRequest(),request)) {
-            reqParametersModel = new HttpRequestParametersHashModel(request);
-            request.setAttribute(ATTR_REQUEST_PARAMETERS_MODEL, reqParametersModel);
-        }
-        model.put(KEY_REQUEST_PARAMETER_MODEL, reqParametersModel);
-        Throwable exception = (Throwable) request.getAttribute("javax.servlet.error.exception");
-        if (Lang.isEmpty(exception)) {
-            exception = (Throwable) request.getAttribute("javax.servlet.error.JspException");
-        }
-        if (!Lang.isEmpty(exception)) {
-            model.put(KEY_EXCEPTION, exception);
-        }
+		if (!Lang.isEmpty(session)) {
+			model.put(KEY_SESSION_MODEL, new HttpSessionHashModel(session,
+					wrapper));
+		}
+		HttpRequestHashModel requestModel = (HttpRequestHashModel) request
+				.getAttribute(ATTR_REQUEST_MODEL);
+		if (Lang.isEmpty(requestModel)
+				|| !Lang.equals(requestModel.getRequest(), request)) {
+			requestModel = new HttpRequestHashModel(request, response, wrapper);
+			request.setAttribute(ATTR_REQUEST_MODEL, requestModel);
+		}
+		model.put(KEY_REQUEST_MODEL, requestModel);
+		HttpRequestParametersHashModel reqParametersModel = (HttpRequestParametersHashModel) request
+				.getAttribute(ATTR_REQUEST_PARAMETERS_MODEL);
+		if (Lang.isEmpty(reqParametersModel)
+				|| !Lang.equals(requestModel.getRequest(), request)) {
+			reqParametersModel = new HttpRequestParametersHashModel(request);
+			request.setAttribute(ATTR_REQUEST_PARAMETERS_MODEL,
+					reqParametersModel);
+		}
+		model.put(KEY_REQUEST_PARAMETER_MODEL, reqParametersModel);
+		Throwable exception = (Throwable) request
+				.getAttribute("javax.servlet.error.exception");
+		if (Lang.isEmpty(exception)) {
+			exception = (Throwable) request
+					.getAttribute("javax.servlet.error.JspException");
+		}
+		if (!Lang.isEmpty(exception)) {
+			model.put(KEY_EXCEPTION, exception);
+		}
 	}
 }
--- src/main/webapp/WEB-INF/main/main_layout.ftl
+++ src/main/webapp/WEB-INF/main/main_layout.ftl
@@ -5,67 +5,67 @@
 <meta http-equiv="X-UA-Compatible" content="IE=7" />
 <title>简单实用国产jQuery UI框架 - DWZ富客户端框架(J-UI.com)</title>
 
-<link href="${base}/resthemes/default/style.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="${base}/resthemes/css/core.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="${base}/resthemes/css/print.css" rel="stylesheet" type="text/css" media="print"/>
-<link href="${base}/resuploadify/css/uploadify.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="${base}/res/themes/default/style.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="${base}/res/themes/css/core.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="${base}/res/themes/css/print.css" rel="stylesheet" type="text/css" media="print"/>
+<link href="${base}/res/uploadify/css/uploadify.css" rel="stylesheet" type="text/css" media="screen"/>
 <!--[if IE]>
 <link href="themes/css/ieHack.css" rel="stylesheet" type="text/css" media="screen"/>
 <![endif]-->
 
-<script src="${base}/resjs/speedup.js" type="text/javascript"></script>
-<script src="${base}/resjs/jquery-1.7.2.js" type="text/javascript"></script>
-<script src="${base}/resjs/jquery.cookie.js" type="text/javascript"></script>
-<script src="${base}/resjs/jquery.validate.js" type="text/javascript"></script>
-<script src="${base}/resjs/jquery.bgiframe.js" type="text/javascript"></script>
-<script src="${base}/resxheditor/xheditor-1.1.14-zh-cn.min.js" type="text/javascript"></script>
-<script src="${base}/resuploadify/scripts/swfobject.js" type="text/javascript"></script>
-<script src="${base}/resuploadify/scripts/jquery.uploadify.v2.1.0.js" type="text/javascript"></script>
+<script src="${base}/res/js/speedup.js" type="text/javascript"></script>
+<script src="${base}/res/js/jquery-1.7.2.js" type="text/javascript"></script>
+<script src="${base}/res/js/jquery.cookie.js" type="text/javascript"></script>
+<script src="${base}/res/js/jquery.validate.js" type="text/javascript"></script>
+<script src="${base}/res/js/jquery.bgiframe.js" type="text/javascript"></script>
+<script src="${base}/res/xheditor/xheditor-1.1.14-zh-cn.min.js" type="text/javascript"></script>
+<script src="${base}/res/uploadify/scripts/swfobject.js" type="text/javascript"></script>
+<script src="${base}/res/uploadify/scripts/jquery.uploadify.v2.1.0.js" type="text/javascript"></script>
 
 <!-- svg图表  supports Firefox 3.0+, Safari 3.0+, Chrome 5.0+, Opera 9.5+ and Internet Explorer 6.0+ -->
-<script type="text/javascript" src="${base}/reschart/raphael.js"></script>
-<script type="text/javascript" src="${base}/reschart/g.raphael.js"></script>
-<script type="text/javascript" src="${base}/reschart/g.bar.js"></script>
-<script type="text/javascript" src="${base}/reschart/g.line.js"></script>
-<script type="text/javascript" src="${base}/reschart/g.pie.js"></script>
-<script type="text/javascript" src="${base}/reschart/g.dot.js"></script>
+<script type="text/javascript" src="${base}/res/chart/raphael.js"></script>
+<script type="text/javascript" src="${base}/res/chart/g.raphael.js"></script>
+<script type="text/javascript" src="${base}/res/chart/g.bar.js"></script>
+<script type="text/javascript" src="${base}/res/chart/g.line.js"></script>
+<script type="text/javascript" src="${base}/res/chart/g.pie.js"></script>
+<script type="text/javascript" src="${base}/res/chart/g.dot.js"></script>
 
-<script src="${base}/resjs/dwz.core.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.util.date.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.validate.method.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.regional.zh.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.barDrag.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.drag.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.tree.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.accordion.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.ui.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.theme.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.switchEnv.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.alertMsg.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.contextmenu.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.navTab.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.tab.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.resize.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.dialog.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.dialogDrag.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.sortDrag.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.cssTable.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.stable.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.taskBar.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.ajax.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.pagination.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.database.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.datepicker.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.effects.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.panel.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.checkbox.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.history.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.combox.js" type="text/javascript"></script>
-<script src="${base}/resjs/dwz.print.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.core.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.util.date.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.validate.method.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.regional.zh.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.barDrag.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.drag.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.tree.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.accordion.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.ui.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.theme.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.switchEnv.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.alertMsg.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.contextmenu.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.navTab.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.tab.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.resize.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.dialog.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.dialogDrag.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.sortDrag.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.cssTable.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.stable.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.taskBar.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.ajax.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.pagination.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.database.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.datepicker.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.effects.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.panel.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.checkbox.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.history.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.combox.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.print.js" type="text/javascript"></script>
 <!--
 <script src="bin/dwz.min.js" type="text/javascript"></script>
 -->
-<script src="${base}/resjs/dwz.regional.zh.js" type="text/javascript"></script>
+<script src="${base}/res/js/dwz.regional.zh.js" type="text/javascript"></script>
 
 <script type="text/javascript">
 $(function(){
